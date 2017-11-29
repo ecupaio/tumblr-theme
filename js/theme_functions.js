3dd76331e2b476c9fd06c9228071b07e57f6b984
Ed Cupaioli <cupaiolie@dnc.org>
2017-11-29 14:44:06 -0500

    no message

--- js/theme_functions.js
+++ js/theme_functions.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    
+    //APi Get posts: https://api.tumblr.com/v2/blog/fuegotakes.tumblr.com/posts/text?api_key=Cpszs4ZMBVOFiP69gZibFzjXTOSNUjACBI9pt1ainu950A8jXL
 });
